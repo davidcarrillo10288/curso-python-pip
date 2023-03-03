21a84b603b8a450a59254cf4a84dd738a1208e23
David <davidicc28@gmail.com>
2023-03-03 12:21:39 -0500

    Agregando pandas a app

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def run():
   '''
 
   df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'Africa']
+  df = df[df['Continent'] == 'South America']
 
   countries = df['Country'].values
   percentages = df['World Population Percentage'].values
